b782d58089150ecb8fd0e8ff13e5b9fe3b20d13c
0xDako <mrepif1337@gmail.com>
2019-03-09 13:20:12 +0300

    First lesson

--- lesson1/test.cpp
+++ lesson1/test.cpp
@@ -1,45 +1,22 @@
-#include <cv.h>
-#include <highgui.h>
-#include <stdlib.h>
-#include <stdio.h>
-//g++ 1.cpp -o main `pkg-config --cflags --libs opencv`
+#include "opencv2/opencv.hpp"
+using namespace cv;
 
-int main(int argc, char* argv[])
+// для компиляции g++ test.cpp -o test `pkg-config --cflags --libs opencv`
+
+int main(int argc, char **argv)
 {
-	// получаем любую подключённую камеру
-	CvCapture* capture = cvCreateCameraCapture(CV_CAP_ANY); //cvCaptureFromCAM( 0 );
-	assert( capture );
-
-	IplImage* frame=0;
-
-	cvNamedWindow("capture", CV_WINDOW_AUTOSIZE);
-
-	printf("[i] press Enter for capture image and Esc for quit!\n\n");
-
-	int counter=0;
-	char filename[512];
-
-	while(true){
-		// получаем кадр
-		frame = cvQueryFrame( capture );
-
-		// показываем
-		cvShowImage("capture", frame);
-
-		char c = cvWaitKey(33);
-		if (c == 27) { // нажата ESC
-			break;
-		}
-		else if(c == 13) { // Enter
-			// сохраняем кадр в файл
-			sprintf(filename, "Image%d.jpg", counter);
-			printf("[i] capture... %s\n", filename);
-			cvSaveImage(filename, frame);
-			counter++;
-		}
-	}
-	// освобождаем ресурсы
-	cvReleaseCapture( &capture );
-	cvDestroyWindow("capture");
-	return 0;
+		//переменная для хранения изображения
+    Mat frame;
+		//переменная для хранения потока видиозахвата
+    VideoCapture cap;
+		//открытие потока(-1 -- любая доступная камера)
+ 		cap.open(-1);
+		// до тех пор пока поток видеозахвата открыт и можно считать изображение выводи его
+    while(cap.isOpened() && cap.read(frame))
+    {
+				//выводим изображение в окно с именем "image"
+        imshow("image",frame);
+				//если нажата клавиша EST то выходим из программы
+        if (waitKey(10)==27) break;
+    }
 }
