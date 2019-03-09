418f383c516202e16284b062fe67e2d68470024b
0xDako <mrepif1337@gmail.com>
2019-03-09 17:12:02 +0300

    First lesson

--- lesson1/exemple1.cpp
+++ lesson1/exemple1.cpp
@@ -1,5 +1,7 @@
 #include "opencv2/opencv.hpp"
+#include <iostream>
 using namespace cv;
+using namespace std;
 
 // для компиляции g++ test.cpp -o test `pkg-config --cflags --libs opencv`
 
@@ -15,10 +17,14 @@ int main(int argc, char **argv)
     while(cap.isOpened() && cap.read(frame))
     {
         Point pt (100,100);
-
-        putText(frame, "Hello", pt, FONT_HERSHEY_SCRIPT_SIMPLEX,2, CV_RGB(100,0,200));
 				//выводим изображение в окно с именем "image"
+        if (int(frame.at<Vec3b>(240,320)[0])>150)putText(frame, "Red apple", pt, FONT_HERSHEY_SCRIPT_SIMPLEX,2, CV_RGB(100,0,200));
+        else if (int(frame.at<Vec3b>(240,320)[1])>150) putText(frame, "Green apple", pt, FONT_HERSHEY_SCRIPT_SIMPLEX,2, CV_RGB(100,0,200));
+        else if (int(frame.at<Vec3b>(240,320)[2])>150) putText(frame, "Blue apple", pt, FONT_HERSHEY_SCRIPT_SIMPLEX,2, CV_RGB(100,0,200));
+        else putText(frame, "Nothing", pt, FONT_HERSHEY_SCRIPT_SIMPLEX,2, CV_RGB(100,0,200));
         imshow("image",frame);
+
+
 				//если нажата клавиша EST то выходим из программы
         if (waitKey(10)==27) break;
     }
