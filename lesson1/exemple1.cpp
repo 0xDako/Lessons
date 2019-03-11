fb731b1f6189c465a09e454b106d164f2bfd3afc
0xDako <mrepif1337@gmail.com>
2019-03-11 17:33:39 +0300

    First lesson

--- lesson1/exemple1.cpp
+++ lesson1/exemple1.cpp
@@ -13,17 +13,22 @@ int main(int argc, char **argv)
     VideoCapture cap;
 		//открытие потока(-1 -- любая доступная камера)
  		cap.open(-1);
+    //точка в которой будет выводится текст
+    Point pt (200,400);
 		// до тех пор пока поток видеозахвата открыт и можно считать изображение выводи его
     while(cap.isOpened() && cap.read(frame))
     {
-        Point pt (100,100);
-				//выводим изображение в окно с именем "image"
-        if (int(frame.at<Vec3b>(240,320)[0])>150)putText(frame, "Red apple", pt, FONT_HERSHEY_SCRIPT_SIMPLEX,2, CV_RGB(100,0,200));
-        else if (int(frame.at<Vec3b>(240,320)[1])>150) putText(frame, "Green apple", pt, FONT_HERSHEY_SCRIPT_SIMPLEX,2, CV_RGB(100,0,200));
-        else if (int(frame.at<Vec3b>(240,320)[2])>150) putText(frame, "Blue apple", pt, FONT_HERSHEY_SCRIPT_SIMPLEX,2, CV_RGB(100,0,200));
-        else putText(frame, "Nothing", pt, FONT_HERSHEY_SCRIPT_SIMPLEX,2, CV_RGB(100,0,200));
+			  //рисуем окружность вокруг точки нанализа
+        circle (frame, cvPoint(320,240),40,CV_RGB(255,0,0),1,1);
+        //анализ изображения 
+        if (int(frame.at<Vec3b>(320,240)[0])>150)putText(frame, "Blue apple", pt, 0,2, CV_RGB(100,0,200),2);
+        else if (int(frame.at<Vec3b>(320,240)[1])>150) putText(frame, "Green apple", pt, 0,2, CV_RGB(100,0,200),2);
+        else if (int(frame.at<Vec3b>(320,240)[2])>150) putText(frame, "Red apple", pt, 0,2, CV_RGB(100,0,200),2);
+        else putText(frame, "Nothing", pt, 0,2, CV_RGB(100,0,200),2);
         imshow("image",frame);
 
+        //вывод всех составляющих на экран
+        //cout << int(frame.at<Vec3b>(320,240)[0]) << " " << int(frame.at<Vec3b>(320,240)[1]) << " " <<int(frame.at<Vec3b>(320,240)[2]) << endl;
 
 				//если нажата клавиша EST то выходим из программы
         if (waitKey(10)==27) break;
