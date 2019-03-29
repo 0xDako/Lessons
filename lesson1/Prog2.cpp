78d8c83f08730d933483f19d51ff911071f0de2d
0xDako <mrepif1337@gmail.com>
2019-03-29 18:05:06 +0300

    First lesson

--- lesson1/Prog2.cpp
+++ lesson1/Prog2.cpp
@@ -19,16 +19,23 @@ int main(int argc, char **argv)
     while(cap.isOpened() && cap.read(frame))
     {
 			  //рисуем окружность вокруг точки нанализа
-        circle (frame, Point(320,240),40,CV_RGB(255,0,0),1,1);
+        circle (frame, Point(320,240),30,CV_RGB(255,0,0),1);
         //анализ изображения
-        if (frame.at<Vec3b>(320,240)[0]>150)putText(frame, "Blue apple", pt, 0,2, CV_RGB(100,0,200),2);
-        else if (frame.at<Vec3b>(320,240)[1]>150) putText(frame, "Green apple", pt, 0,2, CV_RGB(100,0,200),2);
-        else if (frame.at<Vec3b>(320,240)[2]>150) putText(frame, "Red apple", pt, 0,2, CV_RGB(100,0,200),2);
-        else putText(frame, "Nothing", pt, 0,2, CV_RGB(100,0,200),2);
+        if (frame.at<Vec3b>(320,240)[0] - frame.at<Vec3b>(320,240)[1]> 50 &&
+            frame.at<Vec3b>(320,240)[0] - frame.at<Vec3b>(320,240)[2] > 50)
+              putText(frame, "Blue apple", pt, 0,2, CV_RGB(255,255,255),2);
+        else  if(frame.at<Vec3b>(320,240)[1] - frame.at<Vec3b>(320,240)[0]> 40 &&
+                frame.at<Vec3b>(320,240)[1] - frame.at<Vec3b>(320,240)[2] > 40)
+                    putText(frame, "Green apple", pt, 0,2, CV_RGB(255,255,255),2);
+        else if(frame.at<Vec3b>(320,240)[2] - frame.at<Vec3b>(320,240)[0]> 50 &&
+                frame.at<Vec3b>(320,240)[2] - frame.at<Vec3b>(320,240)[1] > 50)
+                    putText(frame, "Red apple", pt, 0,2, CV_RGB(255,255,255),2);
+        else putText(frame, "Nothing", pt, 0,2, CV_RGB(255,255,255),2);
+
         imshow("image",frame);
-        
+
         //вывод всех составляющих на экран
-        //cout << int(frame.at<Vec3b>(320,240)[0]) << " " << int(frame.at<Vec3b>(320,240)[1]) << " " <<int(frame.at<Vec3b>(320,240)[2]) << endl;
+        cout << int(frame.at<Vec3b>(320,240)[0])<< " " <<int(frame.at<Vec3b>(320,240)[1])<< " " <<int(frame.at<Vec3b>(320,240)[2]) << endl;
 
 				//если нажата клавиша EST то выходим из программы
         if (waitKey(10)==27) break;
